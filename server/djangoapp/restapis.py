67fc665e859262261d36d6f7f880a8a821b8564b
rekharn0 <rekharn0@gmail.com>
2024-08-01 15:16:51 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -52,6 +52,7 @@ def analyze_review_sentiments(text):
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
+
     
     request_url = backend_url+"/insert_review"
     try:
