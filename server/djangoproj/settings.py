2308805593e0310880577a12c24e13adf8f38b2d
rekharn0 <rekharn0@gmail.com>
2024-08-01 16:52:43 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,8 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 
                  'https://rekharnmca22-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = 
-                       ['https://rekharnmca22-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+# CSRF_TRUSTED_ORIGINS = ['https://rekharnmca22-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
