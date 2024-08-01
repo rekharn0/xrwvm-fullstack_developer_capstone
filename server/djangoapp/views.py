fd3a5cdb95e4474d4d189442bb7420605d866dc5
rekharn0 <rekharn0@gmail.com>
2024-08-01 17:09:50 +0530

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@
 # from django.shortcuts import render
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
@@ -169,5 +169,3 @@ def get_cars(request):
         cars.append({"CarModel": car_model.name,
                      "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
-    
-     
