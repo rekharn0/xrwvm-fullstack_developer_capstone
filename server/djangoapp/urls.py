a24709f2c0097f1b3f90e3169dd433d0ec48ad98
rekharn0 <rekharn0@gmail.com>
2024-08-01 01:52:05 -0400

    module 4 started done till post review

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('logout',views.logout_request, name='logout'),
 
     # path to get cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name ='get_cars'),
     # path for dealer reviews view
-    #path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     #path dealer details
